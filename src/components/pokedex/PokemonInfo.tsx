551da7d083fa830debeb769fc2585b8edc87bc0b
julietteceb16 <126595208+discovery23@users.noreply.github.com>
2023-09-12 20:29:08 -0600

    cambios2

--- src/components/pokedex/PokemonInfo.tsx
+++ src/components/pokedex/PokemonInfo.tsx
@@ -2,6 +2,7 @@ import { Box, Tab, Tabs } from "@mui/material";
 import "./PokemonInfo.css";
 import React from "react";
 import { Pokemon } from "../../models/Pokemon";
+
 interface TabPanelProps {
     children?: React.ReactNode;
     index: number;
@@ -29,44 +30,71 @@ function CustomTabPanel(props: TabPanelProps) {
 }
 
 interface PokemonInfoProps {
-    pokemon:Pokemon
+    pokemon: Pokemon;
 }
 
-function PokemonInfo(props:PokemonInfoProps) {
+function PokemonInfo(props: PokemonInfoProps) {
     const [value, setValue] = React.useState(0);
     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
         setValue(newValue);
     };
+
     return (
         <div>
             <h1 className="Name">{props.pokemon.name}</h1>
-            <img className="Image" src={props.pokemon.sprites.other["official-artwork"].front_default}></img>
+            <img className="Image" src={props.pokemon.sprites.other["official-artwork"].front_default} alt={props.pokemon.name} />
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                    <Tab style={{color:"white"}} label="Moves" />
-                    <Tab style={{color:"white"}} label="Abilities" />
+                    <Tab style={{ color: "white" }} label="Moves" />
+                    <Tab style={{ color: "white" }} label="Abilities" />
                 </Tabs>
             </Box>
             <CustomTabPanel value={value} index={0}>
-                <div style={{color:"white"}}>
-                    <ul>
-                        {props.pokemon.moves.map((move,index)=>{
-                            return <li key={index}>{move.move.name}</li>
-                        })}
-                    </ul>
+                <div style={{ color: "white" }}>
+                    <div className={`table-container ${props.pokemon.moves.length > 10 ? "with-scroll" : ""}`}>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Move Name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {props.pokemon.moves.map((move, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{move.move.name}</td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </CustomTabPanel>
             <CustomTabPanel value={value} index={1}>
-                <div style={{color:"white"}}>
-                    <ul>
-                        {props.pokemon.abilities.map((ability,index)=>{
-                            return <li key={index}>{ability.ability.name}</li>
-                        })}
-                    </ul>
+                <div style={{ color: "white" }}>
+                    <div className={`table-container ${props.pokemon.abilities.length > 10 ? "with-scroll" : ""}`}>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Ability Name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {props.pokemon.abilities.map((ability, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{ability.ability.name}</td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </CustomTabPanel>
         </div>
-    )
+    );
 }
 
 export default PokemonInfo;
